8e911be09f9cf12cee3dfe22792af2583c118d74
mateusz <m.kolomanski@yahoo.com>
2024-02-27 11:13:16 +0100

    Basic layout and styling

--- src/components/EntrySheet.jsx
+++ src/components/EntrySheet.jsx
@@ -3,16 +3,70 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/EntrySheet.css';
 
-export const EntrySheet = ({id, data, history, layout, pick_lists, field_types, box_data, field_buttons, editable, more_options}) => {
+export const EntrySheet = ({id, layout, data}) => {
+    const entry_id = data[layout["id_field"]];
+    const entry_name = data[layout["name_field"]];
+    const entry_owner = data[layout["owner_field"]];
+
     return (
-      <div className="entry-sheet" id={id}>HELLO</div>
+        <div className="entry-sheet" id={id}>
+            <SheetHeader
+                entry_id={entry_id}
+                entry_name={entry_name}
+                owner={entry_owner}
+            ></SheetHeader>
+
+            {
+                Object.keys(layout['sections']).map((section) => {
+                    return(
+                        <SheetSection
+                            header={section}
+                        ></SheetSection>
+                    )
+                })
+
+            }
+        </div>
     );
 };
 
-EntrySheet.propTypes = {
+const SheetHeader = ({entry_id, entry_name, owner}) => {
+    return (
+        <header class="sheet-header">
+            <input
+                type="text"
+                value={entry_name}
+            ></input>
+            <p>Owned by</p>
+            <input
+                type="text"
+                value={owner}
+            ></input>
+            { entry_id ? <span>Entry ID: {entry_id}</span> : "" }
+        </header>
+    )
+};
 
+const SheetSection = ({header}) => {
+    return(
+        <section class="sheeet-section">
+            <h1>{header}</h1>
+            <div class="">
+                Content
+            </div>
+        </section>
+    )
+}
+
+EntrySheet.propTypes = {
+    id: PropTypes.string,
+    layout: PropTypes.object,
+    data: PropTypes.object
 };
   
 EntrySheet.defaultProps = {
+    id: "",
+    layout: {},
+    data: {}
 
 };
--- src/components/EntrySheet.stories.js
+++ src/components/EntrySheet.stories.js
@@ -3,10 +3,25 @@ import { EntrySheet } from './EntrySheet';
 export default {
     component: EntrySheet,
     parameters: {
-        layout: "centered"
+        layout: "padded"
     }
 };
 
 export const Primary = {
-
+    args: {
+        id: 'test-id',
+        layout: {
+            "id_field": "ID",
+            "name_field": "Entry_name",
+            "owner_field": "Owner",
+            "sections": {
+                "Main": []
+            }
+        },
+        data: {
+            "ID": 1,
+            "Entry_name": "Test entry",
+            "Owner": "Jan Kowalski"
+        }
+    }
 };
